3bb41f983ec30382c5d28a4de1499731cf2c2633
haolinzh <haolinzh@usc.edu>
2016-11-24 12:02:34 -0800

    Nov 24 Morning

---
+++ java/com/example/nova/congressinfo/ComFragment.java
@@ -0,0 +1,12 @@
+package com.example.nova.congressinfo;
+
+import android.support.v4.app.Fragment;
+import android.widget.RelativeLayout;
+
+/**
+ * Created by NOVA on 16/11/23.
+ */
+
+public class ComFragment extends Fragment {
+    RelativeLayout layout;
+}
